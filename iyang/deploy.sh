22b6c5ed455eec809866af9a5e56889a415211ee
yangzhyo <yangzhyo@gmail.com>
2019-06-12 10:43:04 +0800

    新版运行架构

--- iyang/deploy.sh
+++ iyang/deploy.sh
@@ -3,5 +3,9 @@
 cd /app
 git clone --recursive https://github.com/yangzhyo/iyang.git
 cd /app/iyang
+git pull origin master
+cd /app/iyang/themes/next
+git pull origin master
+cd /app/iyang
 npm install
-hexo g
+hexo server
--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -4,6 +4,3 @@ cd /app/iyang
 git pull origin master
 cd /app/iyang/themes/next
 git pull origin master
-cd /app/iyang
-npm install
-hexo g
