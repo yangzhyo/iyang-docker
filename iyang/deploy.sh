0d1e11ba402bfd4c8a3be03e0eb5de94299f54dc
yangzhyo <yangzhyo@gmail.com>
2019-06-30 17:50:47 +0800

    add iyang-asset

--- iyang/deploy.sh
+++ iyang/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 cd /app
+git clone https://github.com/yangzhyo/iyang-asset.git
+cd /app/iyang-asset
+git pull origin master
+cd /app
 git clone --recursive https://github.com/yangzhyo/iyang.git
 cd /app/iyang
 git pull origin master
--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -4,3 +4,5 @@ cd /app/iyang
 git pull origin master
 cd /app/iyang/themes/next
 git pull origin master
+cd /app/iyang-asset
+git pull origin master
